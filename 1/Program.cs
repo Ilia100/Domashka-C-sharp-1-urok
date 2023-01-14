5f07b19c2979987a3f97a7a687d8d1dee4b4826d
ilia <ilia100@hotmail.com>
2023-01-14 21:22:16 +0300

    first

---
+++ 1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("this program  returns the square of a number!");
+Console.WriteLine("insert a number:");
+string numberstr = Console.ReadLine();
+int numberint = Convert.ToInt32(numberstr);
+int result = numberint * numberint;
+Console.WriteLine($"the square of {numberstr} = {result}");
+Console.WriteLine("the square of " + numberstr + " = " + result);
+Console.WriteLine("the square of {0} = {1}", numberstr, result);
