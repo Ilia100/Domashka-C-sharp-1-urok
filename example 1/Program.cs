998768c1126fd7821c0f269678124dec6490916d
ilia <ilia100@hotmail.com>
2023-01-15 12:19:23 +0300

    переделаны папки

--- example 1/Program.cs
+++ example 1/Program.cs
@@ -1,4 +1,14 @@
-﻿Console.WriteLine("this program  returns the square of a number!");
+﻿/*
+Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное
+на само себя).
+
+Например:
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+
+Console.WriteLine("this program  returns the square of a number!");
 Console.WriteLine("insert a number:");
 string numberstr = Console.ReadLine();
 int numberint = Convert.ToInt32(numberstr);
---
+++ задача 4/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("какое число больше");
+Console.WriteLine("напишите 1 число:");
+string num1 = Console.ReadLine();
+Console.WriteLine("напишите 2 число:");
+string num2 = Console.ReadLine();
+Console.WriteLine("напишите 3 число:");
+string num3 = Console.ReadLine();
+
+int num1int = Convert.ToInt32(num1);
+int num2int = Convert.ToInt32(num2);
+int num3int = Convert.ToInt32(num3);
+int result = 0;
+if (num1int == num2int)
+{
+ result = num1int;
+ Console.WriteLine("числа одинаковые");
+}
+else
+{
+   if (num1int > num2int)
+   {
+   result = num1int;
+   Console.WriteLine("большее число " + result);
+   }
+   else
+   {
+   result = num2int;
+   Console.WriteLine("большее число " + result);
+   }
+}
+ 
