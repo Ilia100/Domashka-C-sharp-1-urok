ab859c010eb04fbc21883904258f275bca28a682
ilia <ilia100@hotmail.com>
2023-01-15 13:32:13 +0300

    6 done

--- задача 7/Program.cs
+++ задача 7/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача №7. Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает последнюю цифру этого числа.
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int a = Convert.ToInt32(number);
+if (a>999 || a<100)
+{
+Console.WriteLine ("число не трехзначное");
+}
+else 
+{int b = a % 10;
+Console.WriteLine ("последнее число " + b);
+}
