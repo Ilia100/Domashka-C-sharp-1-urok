f6b88fc8a2f69ac754b9454818366f50bb596e0d
ilia <ilia100@hotmail.com>
2023-01-15 15:10:13 +0300

    сделал 8 задачу

--- задача 8/Program.cs
+++ задача 8/Program.cs
@@ -4,3 +4,19 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
+Console.WriteLine("Введите положительное целое число больше 0");
+string numberStr = Console.ReadLine(); 
+int number = Convert.ToInt32(numberStr); 
+
+if (number<1)
+{
+    Console.Write("число не соответствует условиям задачи"); 
+}
+else
+{
+for (int i = 1; i <= number; i++)
+{
+ if (i%2==0)
+ Console.Write(i + ", ");
+}
+}
